33dc7473836477a6e133e159c50526a7c1cafc40
OBAID ULLAH KHAN <obaidullahkhankhail@gmail.com>
2022-11-16 12:15:37 +0500

    middlewares

--- server.go
+++ server.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"golang-gin-poc/controller"
+	"golang-gin-poc/middlewares"
 	"golang-gin-poc/service"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -12,19 +16,31 @@ var (
 	VideoController controller.VideoController = controller.New(videoService)
 )
 
+func setuupLogOutput() {
+	file, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	// server := gin.Default()
 	// server.GET("/test", func(ctx *gin.Context) {
 	// 	ctx.JSON(200, gin.H{
 	// 		"message":"hi this is my code",
 	// 	})
 	// })
 
-	server.GET("/videos", func(ctx *gin.Context){
+	setuupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
+	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, VideoController.FindAll())
 	})
 
-	server.POST("/videos", func(ctx *gin.Context){
+	server.POST("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, VideoController.Save(ctx))
 	})
 
